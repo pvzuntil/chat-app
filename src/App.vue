10c22911215f87e171297608b24d3639fe8faef9
untillness <pvauntil@gmail.com>
2020-10-10 14:34:48 +0700

    change: structure

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,14 @@
 <template>
   <v-app>
-    <SystemBar />
-    <AppBar />
-    <ListChat />
-    <RightSidebar />
-
-    <v-main>
-      <v-container fluid>
-        <span v-for="n in 10" :key="n + 'L'">
-          <v-hover v-slot:default="{ hover }" open-delay="400">
-            <span>
-              <v-row class="px-8 mt-2 d-flex">
-                <v-card class="px-3 py-1" elevation="1" color="blue lighten-4">
-                  sdhsjdhj
-                </v-card>
-              </v-row>
-              <v-expand-transition>
-              <v-row class="px-8 mt-1 d-flex" v-if="hover">
-                <span style="font-size: 12px">10 Desember 1020 - 20:10</span>
-              </v-row>
-              </v-expand-transition>
-            </span>
-          </v-hover>
-        </span>
-
-        <span v-for="n in 10" :key="n + 'R'">
-          <v-row class="px-8 mt-2 d-flex justify-end">
-            <v-card class="px-3 py-1" elevation="1" color="blue lighten-2">
-              sdhsjdhj
-            </v-card>
-          </v-row>
-          <v-row class="px-8 mt-1 d-flex justify-end">
-            <span style="font-size: 12px">10 Desember 1020 - 20:10</span>
-          </v-row>
-        </span>
-      </v-container>
-    </v-main>
-
-    <ChatField />
+    <router-view />
   </v-app>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld';
-import SystemBar from "./components/SystemBar";
-import AppBar from "./components/AppBar";
-import ListChat from "./components/ListChatSidebar";
-import RightSidebar from "./components/RightSidebar";
-import ChatField from "./components/ChatField";
-
 export default {
   name: "App",
 
-  components: {
-    SystemBar,
-    AppBar,
-    ListChat,
-    RightSidebar,
-    ChatField,
-  },
-
-  data: () => ({
-    drawer: true,
-  }),
+  data: () => ({}),
 
   mounted() {},
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,64 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <SystemBar />
+    <AppBar />
+    <ListChat />
+    <RightSidebar />
+
+    <v-main>
+      <v-container fluid>
+        <span v-for="n in 10" :key="n + 'L'">
+          <v-hover v-slot:default="{ hover }" open-delay="400">
+            <span>
+              <v-row class="px-8 mt-2 d-flex">
+                <v-card class="px-3 py-1" elevation="1" color="blue lighten-4">
+                  sdhsjdhj
+                </v-card>
+              </v-row>
+              <v-expand-transition>
+              <v-row class="px-8 mt-1 d-flex" v-if="hover">
+                <span style="font-size: 12px">10 Desember 1020 - 20:10</span>
+              </v-row>
+              </v-expand-transition>
+            </span>
+          </v-hover>
+        </span>
+
+        <span v-for="n in 10" :key="n + 'R'">
+          <v-row class="px-8 mt-2 d-flex justify-end">
+            <v-card class="px-3 py-1" elevation="1" color="blue lighten-2">
+              sdhsjdhj
+            </v-card>
+          </v-row>
+          <v-row class="px-8 mt-1 d-flex justify-end">
+            <span style="font-size: 12px">10 Desember 1020 - 20:10</span>
+          </v-row>
+        </span>
+      </v-container>
+    </v-main>
+
+    <ChatField />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import SystemBar from "../components/SystemBar";
+import AppBar from "../components/AppBar";
+import ListChat from "../components/ListChatSidebar";
+import RightSidebar from "../components/RightSidebar";
+import ChatField from "../components/ChatField";
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    SystemBar,
+    AppBar,
+    ListChat,
+    RightSidebar,
+    ChatField,
+  },
 }
 </script>
